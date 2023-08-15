7ae724c7669c927efcd51615018d61032262ef9a
Sem Stassen <semstassen@gmail.com>
2023-08-15 16:07:16 +0200

    Change DB Tweet model to reference a user

--- src/components/Ui/Tweets.tsx
+++ src/components/Ui/Tweets.tsx
@@ -8,7 +8,7 @@ export default async function Tweets({ tweets }: TweetsProps) {
   return (
     <ul>
       {tweets.map((t) => (
-        <li key={t.id} className="border px-4 py-6">
+        <li key={t.id} className="border px-4 py-6 text-white">
           {t.content}
         </li>
       ))}
