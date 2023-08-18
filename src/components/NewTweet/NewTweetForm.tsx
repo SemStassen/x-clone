383244eddf7fdf3e3141f7b3fbb7e697c2890d62
Sem Stassen <semstassen@gmail.com>
2023-08-18 17:27:36 +0200

    Check and limit max-characters of new tweet

--- src/components/NewTweet/NewTweetForm.tsx
+++ src/components/NewTweet/NewTweetForm.tsx
@@ -4,6 +4,8 @@ import { useForm } from "react-hook-form";
 import { Button } from "../General";
 import { useRouter } from "next/navigation";
 import toast from "react-hot-toast";
+import { useRef, useState } from "react";
+import { text } from "stream/consumers";
 
 interface FormData {
   content: string;
@@ -35,12 +37,23 @@ export default function NewTweetForm() {
     }
   };
 
+  const textAreaRef = useRef<HTMLTextAreaElement>(null);
+
+  const checkCharacterLimit = () => {
+    if (textAreaRef.current === null) return;
+    textAreaRef.current.value = textAreaRef.current.value.slice(0, 255);
+  };
+
   return (
     <form onSubmit={handleSubmit(onSubmit)} className="text-white">
       <textarea
         className="w-full bg-black text-xl focus-visible:outline-none"
         placeholder="What is happening?"
         {...register("content", { required: true })}
+        ref={textAreaRef}
+        onInput={() => {
+          checkCharacterLimit();
+        }}
       ></textarea>
       {errors.content && toast.error("Tell us your tweet first")}
       <Button type="submit" className="ms-auto block">
