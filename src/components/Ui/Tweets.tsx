953900f073bc81f376c71e4b95163a3f2aff369f
Sem Stassen <semstassen@gmail.com>
2023-08-21 12:14:14 +0200

    prevent going to Tweet when clicking toolbarItem

--- src/components/Ui/Tweets.tsx
+++ src/components/Ui/Tweets.tsx
@@ -1,6 +1,9 @@
+"use client";
+
 import type { Like, Tweet as TweetType, User } from "@prisma/client";
-import Link from "next/link";
 import { Tweet } from "@/components/Ui";
+import { useRouter } from "next/navigation";
+import { isFloat64Array } from "util/types";
 
 export interface TweetWithUser extends TweetType {
   user: User;
@@ -14,14 +17,41 @@ interface TweetsProps {
   tweets: Array<TweetWithUser>;
 }
 
-export default async function Tweets({ tweets }: TweetsProps) {
+export default function Tweets({ tweets }: TweetsProps) {
+  const router = useRouter();
+
+  const routeToTweet: (
+    event: React.MouseEvent<HTMLAnchorElement, MouseEvent>,
+    tweetId: TweetWithUser["id"],
+  ) => any = (event, tweetId) => {
+    event.preventDefault();
+
+    const tweet = event.currentTarget;
+    const toolbarItems = Array.from(
+      tweet.querySelectorAll("[data-toolbar-item]"),
+    );
+
+    for (const item of toolbarItems) {
+      if (item.contains(event.target as Node)) {
+        return;
+      }
+    }
+
+    router.push(`/status/${tweetId}`);
+  };
+
   return (
     <ul>
       {tweets.map((t) => (
         <li key={t.id}>
-          <Link href={`status/${t.id}`}>
+          <a
+            href={`status/${t.id}`}
+            onClick={(e) => {
+              routeToTweet(e, t.id);
+            }}
+          >
             <Tweet tweet={t}></Tweet>
-          </Link>
+          </a>
         </li>
       ))}
     </ul>
