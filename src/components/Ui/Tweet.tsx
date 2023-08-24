545ceea0c0efe30c2fa38ce573d1160f041d5286
Sem Stassen <semstassen@gmail.com>
2023-08-24 17:50:15 +0200

    Add top bar with conditional backwards navigation

--- src/components/Ui/Tweet.tsx
+++ src/components/Ui/Tweet.tsx
@@ -1,4 +1,4 @@
-import { TweetToolBar } from "../General";
+import { TweetToolBar } from "../Ui";
 import { TweetWithUser } from "./Tweets";
 
 interface TweetProps {
@@ -8,7 +8,10 @@ interface TweetProps {
 export default function Tweet({ tweet }: TweetProps) {
   return (
     <article className="border px-4 py-6 text-white">
-      <small>@{tweet.user.handle}</small>
+      <div className="mb-2 flex flex-col">
+        <h3 className="text-lg font-bold">{tweet.user.profile?.username}</h3>
+        <small className=" text-xs text-slate-400">@{tweet.user.handle}</small>
+      </div>
       <p className="  break-all">{tweet.content}</p>
       <TweetToolBar
         id={tweet.id}
--- src/components/Ui/index.ts
+++ src/components/Ui/index.ts
@@ -1,2 +1,5 @@
 export { default as Tweets } from "./Tweets";
 export { default as Tweet } from "./Tweet";
+export { default as TweetToolBar } from "./TweetToolBar";
+export { default as NavBar } from "./NavBar";
+export { default as TopBar } from "./TopBar";
