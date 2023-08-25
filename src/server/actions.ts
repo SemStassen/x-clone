92e64761dca98c87334a9640f345ad2b73019b33
Sem Stassen <semstassen@gmail.com>
2023-08-25 13:30:04 +0200

    add caching

--- src/server/actions.ts
+++ src/server/actions.ts
@@ -1,44 +1,48 @@
 import { cache } from "react";
 import { prisma } from "./prisma";
 
-const getTweetsByAmount = async (session: any, amount: number = 10) =>
-  await prisma.tweet.findMany({
-    take: amount,
-    orderBy: { createdAt: "desc" },
-    include: {
-      user: true,
-      _count: {
-        select: {
-          likes: true,
+const getTweetsByAmount = cache(
+  async (session: any, amount: number = 10) =>
+    await prisma.tweet.findMany({
+      take: amount,
+      orderBy: { createdAt: "desc" },
+      include: {
+        user: true,
+        _count: {
+          select: {
+            likes: true,
+          },
+        },
+        likes: {
+          where: {
+            userId: session?.user.userId,
+          },
         },
       },
-      likes: {
-        where: {
-          userId: session?.user.userId,
-        },
-      },
-    },
-  });
+    }),
+);
 
-const getTweetById = async (session: any, tweetId: string) =>
-  await prisma.tweet.findUnique({
-    where: {
-      id: tweetId,
-    },
-    include: {
-      user: true,
-      _count: {
-        select: {
-          likes: true,
+const getTweetById = cache(
+  async (session: any, tweetId: string) =>
+    await prisma.tweet.findUnique({
+      where: {
+        id: tweetId,
+      },
+      include: {
+        user: true,
+        _count: {
+          select: {
+            likes: true,
+          },
+        },
+        likes: {
+          where: {
+            userId: session?.user.userId,
+          },
         },
       },
-      likes: {
-        where: {
-          userId: session?.user.userId,
-        },
-      },
-    },
-  });
+    }),
+);
 
 const getUserByHandle = cache(
   async (handle: string) =>
